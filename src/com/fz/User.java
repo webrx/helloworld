372e5a64101bac15ccbdc757d4825ab4f5326e90
webrx <webrx@126.com>
2017-08-21 09:47:52 +0800

    Initial commit

---
+++ src/com/fz/User.java
@@ -0,0 +1,10 @@
+package com.fz;
+
+/**
+ * Created by webrx on 2017-08-21.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world...");
+    }
+}
