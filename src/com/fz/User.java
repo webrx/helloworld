15d4adc6c997566d29c3fe4717b3d155e96217d0
webrx <webrx@126.com>
2017-08-21 09:48:50 +0800

    my hello

--- src/com/fz/User.java
+++ src/com/fz/User.java
@@ -6,5 +6,13 @@ package com.fz;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
+        System.out.println("hello world...");
     }
 }
